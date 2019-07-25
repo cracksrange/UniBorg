11f3d8ca580b8eda03bedb51a5a456209f37797c
ananduks <52703105+ananduks@users.noreply.github.com>
2019-07-25 06:27:39 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -16,9 +16,9 @@ borg.storage.PREV_REPLY_MESSAGE = {}
 
 
 BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
-TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
+TG_COMPANION_USER_BOT = "Anandus is a busy man dont disturb അട മോനെ ലോക്ക ഇങ്ങ് പോര് ഇങ്ങ് പോര്."
 UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
-UNIBORG_USER_BOT_NO_WARN = "Hi! I will answer to your message soon. Please wait for my response and don't spam my PM. Thanks"
+UNIBORG_USER_BOT_NO_WARN = "wait for a second i will reply soon ."
 
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
