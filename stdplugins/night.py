eb47f8d0b5f41e8ab0325401657d82a51fb0e272
ananduks <52703105+ananduks@users.noreply.github.com>
2019-07-25 20:29:55 +0530

    Add files via upload

---
+++ stdplugins/night.py
@@ -0,0 +1,38 @@
+# BY @STARKTM1
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.night", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+ 
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("Good😍 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \nnight😴😴 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \neveryone😴😴 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \nsleep \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n വെൽ \n")
+    await asyncio.sleep(1)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n😒😒😒😒 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n@zmovies10\n😘😘😘😘 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n😍😍😍😍 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("LOVE U 😘😘😘...")
+    await asyncio.sleep(2)
+    await event.delete()
+    
+    
+
+    
